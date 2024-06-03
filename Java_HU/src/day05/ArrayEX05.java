99db8db3697473b93a6d537c1281bac0100a2621
HU2232 <sujifi@naver.com>
2024-06-03 21:52:27 +0900

    수업내용 수정

--- Java_HU/src/day05/ArrayEX05.java
+++ Java_HU/src/day05/ArrayEX05.java
@@ -7,22 +7,26 @@ public class ArrayEX05 {
 	public static void main(String[] args) {
 				
 		//1~10사이의 랜덤한 수 3개를 저장하고 출력하는 예제 작성
-		//1 3 2
+		//예시 : 랜덤 생성 숫자1 3 2
+		
+		int [] arr  = new int[3];
+		int min = 1, max = 10;		
+		for(int i=0; i<arr.length; i++) {
+			arr [i]= (int)(Math.random() * (max - min +1) + min);
+			System.out.print(arr[i] + "");
+		}
+		System.out.println();
 		
 		//숫자를 입력해서 입력한 숫자가 랜덤한 수에 있는지 확인하는 예제
-		//8 입력시 없습니다
-		//2 입력시 있습니다 
+		//8 입력시 8은 없습니다
+		//2 입력시 2는 있습니다 
 		
 		//정수 입력
-		int num [] = new int[3];
-		int min = 1, max = 10;		
-		for(int i=0; i<=num.length; i++) {
-			int ran = (int)(Math.random() * (max - min +1) + min);
-			System.out.println(num[i] + "");
-			
+	
 			Scanner scan = new Scanner(System.in);
-			System.out.println("정수 입력 ("+min+"~"+max+") : ");
-		}
+			System.out.println("정수 입력 (" + min +" ~ "+ max + ") : ");
+			int num = scan.nextInt();
+		
 		/* 방법1
 		 * 정수 3개가 있는데 내가 찾으려는 정수의 위치가 어딘지 확인.
 		 * 중간에 찾았으면 1~3사이이에 위치할테고 없으면 3보다 크다
@@ -32,17 +36,23 @@ public class ArrayEX05 {
 		 * 반복문 종료 후 : i가 배열의 크기보다 작으면 ~은 있습니다. 라고 출력 같으면 없습니다. 라고 출력
 		 * */
 		int i;
-		for(i=0; i<num.length; i++) {
-			if(ran[i] == num){
-				System.out.println();
+		for(i=0; i<arr.length; i++) {
+			if(arr[i] == num){
+				//있다
 				break;
 			}
 		}
-		if( i < num.length) {
-			System.out.println(num + "는 있습니다.");
+		/*else if( i == num.length - 1) {
+			//없다
 		}
-		
-		
+		* 위의 코드도 입력할 순 있으나 추천하진 않음
+		*/
+		if( i < arr.length) {
+			System.out.println(num + "는 있습니다");
+		}
+		else {
+			System.out.println(num + "는 없습니다");
+		}
 			
 		/* 방법2
 		 * 반복횟수 : 향상된 for문을 이용하여 전체 탐색

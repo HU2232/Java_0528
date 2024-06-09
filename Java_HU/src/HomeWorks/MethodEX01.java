4d8cd3fe7493c1c8c4690260adc36b57d7235f69
HU2232 <sujifi@naver.com>
2024-06-09 18:59:02 +0900

    전혀 모르겠다 쉬발

--- Java_HU/src/HomeWorks/MethodEX01.java
+++ Java_HU/src/HomeWorks/MethodEX01.java
@@ -1,16 +1,25 @@
 package HomeWorks;
 
+import java.util.Scanner;
+
 public class MethodEX01 {
 
 	public static void main(String[] args) {
 		/* 정수 n을 입력받아 입력받은 n 크기를 가지는 배열을 생성하는 코드를 작성하시오
 		 * 메서드 이용*/
+		Scanner scan = new Scanner(System.in);
+		
 
 	}
-	/** 기능 :
-	 *  매개변수 :
-	 *  리턴타입 :
-	 *  매서드명 :
-	 *  */
+	/** 기능 : 정수   n 을 입력받으면  n만큼의 크기를 가지는 배열을 생성함
+	 *  매개변수 : 배열 크기 => n
+	 *  리턴타입 : 정수 배열 arr, 정수 n => int [] arr = new int [n], num n
+	 *  매서드명 : arrays
+	 *  */ 
 
+
+	public static int arrays (int n) {
+		return n;
+		
+	}
 }
--- Java_HU/src/HomeWorks/MethodEX02.java
+++ Java_HU/src/HomeWorks/MethodEX02.java
@@ -7,10 +7,15 @@ public class MethodEX02 {
 		 * 메서드 이용*/
 
 	}
-	/** 기능 :
-	 *  매개변수 :
-	 *  리턴타입 :
-	 *  매서드명 :
+	/** 기능 : 1~9사이의 랜덤한 수를 생성하는 메서드
+	 *  매개변수 : 최소값 : int num min, 최대값 : int num max
+	 *  리턴타입 : 1 ~ 9 사이의 랜덤한 수 : int ran
+	 *  매서드명 : random
 	 *  */
-
+	
+	/** 기능 : 배열 중복되지 않게 생성하는 메서드
+	 *  매개변수 : 최소값 : int min, 최대값 : int max, 배열 : int [] arr  = new int[];
+	 *  리턴타입 : 1 ~ 9 사이의 랜덤한 수가 들어있는 배열 : int [] arr  = new int[ran];
+	 *  매서드명 : randomArray
+	 *  */
 }
